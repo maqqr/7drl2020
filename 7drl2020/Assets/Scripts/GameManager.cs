a93c8f39e7c1d33cb8f32d0f91130d8bcf05f0f8
Miikka Kilkkilä <minuns@gmail.com>
2020-03-01 20:44:54 +0200

    basis for mouse catching

--- 7drl2020/Assets/Scripts/GameManager.cs
+++ 7drl2020/Assets/Scripts/GameManager.cs
@@ -4,17 +4,26 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    private Camera cam;
     public LevelGenerator.Scripts.LevelGenerator levelGeneratorPrefab;
 
     Vector2Int TileCoordinateUnderMouse()
     {
         // TODO: raycast from camera
-
-        return new Vector2Int(0, 0);
+        RaycastHit hit;
+        Vector3 objectHit = new Vector3(0,0,0);
+        Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+        if (Physics.Raycast(ray, out hit)) {
+            objectHit = hit.point;
+            
+            // Do something with the object that was hit by the raycast.
+        }
+        return new Vector2Int((int)objectHit.x, (int)objectHit.z);
     }
 
     void Start()
     {
+        cam = Camera.main;
         // This does not work yet.
         //levelGeneratorPrefab.GenerateLevel();
     }
@@ -29,6 +38,7 @@ public class GameManager : MonoBehaviour
         Vector2Int tile = TileCoordinateUnderMouse();
 
         Vector3 unityCoordinate = new Vector3(tile.x + 0.5f, 0.0f, tile.y + 0.5f);
+        Debug.Log("X: " + tile.x + ", Y: " + tile.y);
 
         Gizmos.DrawWireSphere(unityCoordinate, 0.5f);
     }
