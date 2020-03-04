c78883270c5c479a6538a9532981386e39230c2d
Simo Rinne <rinne.simo@gmail.com>
2020-03-04 21:05:08 +0200

    Added flies

---
+++ 7drl2020/Assets/Scripts/RotateConstantly.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace Verminator
+{
+    public class RotateConstantly : MonoBehaviour
+    {
+        public Vector3 Axis;
+        public float Speed;
+
+        void Update()
+        {
+            transform.Rotate(Axis, Speed * Time.deltaTime);
+        }
+    }
+}
