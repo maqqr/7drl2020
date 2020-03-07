a357c07c98a2951263a04e52cfe6876420a57169
Simo Rinne <rinne.simo@gmail.com>
2020-03-07 15:47:26 +0200

    Added some sounds

---
+++ 7drl2020/Assets/Scripts/SoundEffects.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Verminator
+{
+    public class SoundEffects : MonoBehaviour
+    {
+        public AudioClip[] FootStep;
+        public AudioClip[] Miss;
+        public AudioClip[] Hit;
+
+        public AudioClip OpenInventory;
+        public AudioClip CloseInventory;
+
+        public AudioClip Lantern;
+
+        private AudioSource source;
+
+        private void Awake()
+        {
+            source = GetComponent<AudioSource>();
+        }
+
+        public void PlayClip(AudioClip clip)
+        {
+            source.PlayOneShot(clip);
+        }
+
+        public void PlayClip(AudioClip[] clips)
+        {
+            int index = Random.Range(0, clips.Length);
+            source.PlayOneShot(clips[index]);
+        }
+    }
+}
